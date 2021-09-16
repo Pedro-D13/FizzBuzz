493b814da760c52c4743f0dcad61d0fbce8a9e50
Pedro-D13 <p.afonsosd1994@gmail.com>
2021-09-16 22:35:57 +0100

    FizzBuzz works through current implementation

--- BasePackage/src/FizzBuzz.java
+++ BasePackage/src/FizzBuzz.java
@@ -1,15 +1,37 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class FizzBuzz {
-    public static void main (String[] args){
-        System.out.println("Hello anything");
-        countTo100();
+    public static void main(String[] args) {
+        countTo();
+
     }
-    public static void countTo100(){
+
+
+    public static void countTo() {
+        Map<Integer, String> keyNumberDict = new HashMap<Integer, String>();
+        keyNumberDict.put(3, "Fizz");
+        keyNumberDict.put(5, "Buzz");
+        ArrayList<String> finalArray = new ArrayList<String>();
         for (int i = 1; i < 101; i++) {
-            if (i % 3 == 0){
-                System.out.println("Fizz");
-            } else {
+            String result = "";
+            for (Integer num : keyNumberDict.keySet()) {
+                result += (i % num == 0) ? keyNumberDict.get(num) : "";
+            }
+
+            if (result.isEmpty()) {
                 System.out.println(i);
+            } else {
+                System.out.println(result);
             }
         }
+
+
     }
+
 }
