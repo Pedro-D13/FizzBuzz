f35eed7a66e9469acf402801be4a550d6ca4fd43
beckythebotato <beckyrderrick@gmail.com>
2021-09-16 14:59:13 +0100

    Multiples of three print out Fizz

--- BasePackage/src/FizzBuzz.java
+++ BasePackage/src/FizzBuzz.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class FizzBuzz {
+public class FizzBuzz {
+    public static void main (String[] args){
+        System.out.println("Hello anything");
+        countTo100();
+    }
+    public static void countTo100(){
+        for (int i = 1; i < 101; i++) {
+            if (i % 3 == 0){
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
